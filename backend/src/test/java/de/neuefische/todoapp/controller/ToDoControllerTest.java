8018a0ce84b1fbc818d7ed842811dfa8195cbe61
Wladi Moser <wladi.moser@gmail.com>
2020-05-30 13:40:22 +0200

    Added another integration test for putmapping

--- backend/src/test/java/de/neuefische/todoapp/controller/ToDoControllerTest.java
+++ backend/src/test/java/de/neuefische/todoapp/controller/ToDoControllerTest.java
@@ -1,5 +1,6 @@
 package de.neuefische.todoapp.controller;
 
+import de.neuefische.todoapp.data.Description;
 import de.neuefische.todoapp.db.ToDoNoteDB;
 import de.neuefische.todoapp.enums.Status;
 import de.neuefische.todoapp.models.ToDoNote;
@@ -9,9 +10,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.List;
+
+import static de.neuefische.todoapp.enums.Status.OPEN;
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ToDoControllerTest {
@@ -34,8 +40,7 @@ class ToDoControllerTest {
     public void addingAnItemToDatabaseShouldDisplayItemOnFrontend() {
 
         //GIVEN
-        toDoNoteDB.addNoteInBackend(new ToDoNote("34","testy test", Status.OPEN));
-
+        toDoNoteDB.addNoteInBackend(new ToDoNote("34","testy test", OPEN));
 
         //WHEN
         ResponseEntity<ToDoNote[]> response = restTemplate.getForEntity("http://localhost:" + port + "/api/todo", ToDoNote[].class);
@@ -43,8 +48,31 @@ class ToDoControllerTest {
         ToDoNote[] notes = response.getBody();
 
         //THEN
-        assertTrue(notes[0].getDescription().equals("testy test"));
+        assertEquals("testy test", notes[0].getDescription());
         assertEquals(HttpStatus.OK, statusCode);
+        assertEquals(1, notes.length);
 
     }
+
+    @Test
+    public void addingANoteInFrontendShouldPlaceThisItemInDatabase() {
+
+        //GIVEN
+        Description newNote = new Description("I need to test my Putmapping");
+        HttpEntity<Description> requestEntity = new HttpEntity<>(newNote);
+
+        //WHEN
+        ResponseEntity<ToDoNote> response = restTemplate.exchange("http://localhost:" + port + "/api/todo",HttpMethod.PUT, requestEntity, ToDoNote.class);
+        HttpStatus statusCode = response.getStatusCode();
+        ToDoNote notes = response.getBody();
+
+        //THEN
+        assertEquals(1, toDoNoteDB.getAllNotes().size());
+        assertEquals(OPEN, notes.getStatus());
+        assertEquals(HttpStatus.OK, statusCode);
+        assertTrue(toDoNoteDB.getAllNotes().contains(notes));
+        assertEquals("I need to test my Putmapping", notes.getDescription());
+    }
+
+
 }
